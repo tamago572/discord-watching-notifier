8a4e0414822cd108b74b9c52e4629260d5ac3cf3
tamago572 <110117095+tamago572@users.noreply.github.com>
2026-04-17 17:22:08 +0900

    urlによる分岐処理を追加

--- chromeExt/src/content/content.ts
+++ chromeExt/src/content/content.ts
@@ -1,13 +1,32 @@
 import type { PageMatchedMessage } from "../types/NMProps"
+import type WatchingSiteStatus from "../types/watchingSitesStatus";
+
+const url: string = window.location.origin + window.location.pathname;
+const watchingSitesStatus: WatchingSiteStatus = {
+	title: "",
+	description: "",
+	url: "",
+}
+
+switch (url) {
+	case "https://animestore.docomo.ne.jp/animestore/sc_d_pc":
+		watchingSitesStatus.url = "https://animestore.docomo.ne.jp/";
+		watchingSitesStatus.title = document.querySelectorAll(".backInfoTxt1")[0].textContent || "";
+		watchingSitesStatus.description = `${document.querySelectorAll(".backInfoTxt2")[0].textContent || ""} ${document.querySelectorAll(".backInfoTxt3")[0].textContent || ""}`;
+		break;
+	case "https://www.amazon.co.jp/gp/video/detail":
+		watchingSitesStatus.url = "https://www.amazon.co.jp/";
+		// TODO: Amazon Prime VideoのHTML構造から取得する処理を書く
+
+		break;
+	default:
+		break;
+}
 
 // このページが matches に入ったので、URL と title を background に渡す。
 const message: PageMatchedMessage = {
 	type: "PAGE_MATCHED",
-	url: "https://animestore.docomo.ne.jp/",
-	// アニメタイトル
-	title: document.querySelectorAll(".backInfoTxt1")[0].textContent || "",
-	// EP01 話タイトル
-	description: `${document.querySelectorAll(".backInfoTxt2")[0].textContent || ""} ${document.querySelectorAll(".backInfoTxt3")[0].textContent || ""}`
+	...watchingSitesStatus,
 }
 
 console.log("[Discord Watching Notifier] Page matched:", message.url, message.title);
---
+++ chromeExt/src/types/watchingSitesStatus.ts
@@ -0,0 +1,5 @@
+export default interface WatchingSiteStatus {
+  url: string;
+  title: string;
+  description: string;
+}
