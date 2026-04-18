28556b3561ce4874e36dd876f3920058700c6f83
tamago572 <110117095+tamago572@users.noreply.github.com>
2026-04-19 02:25:26 +0900

    スクリプトの実行が早すぎて取得できない問題を修正

--- chromeExt/src/content/content.ts
+++ chromeExt/src/content/content.ts
@@ -8,11 +8,33 @@ const watchingSitesStatus: WatchingSiteStatus = {
 	url: "",
 }
 
+const sendToBackground = () => {
+	// このページが matches に入ったので、URL と title を background に渡す。
+	const message: PageMatchedMessage = {
+		type: "PAGE_MATCHED",
+		...watchingSitesStatus,
+	}
+
+	console.log("[Discord Watching Notifier] Page matched:", message.url, message.title);
+
+	// content script からは native messaging を直接呼べないので、service worker に中継する。
+	// タブが閉じられることを監視したいため、Service WorkerとContent Scriptをつなぐポートを作る。
+	// ポートはページ遷移やタブ閉じで切断されるため、それで検知する
+	const backgroundPort = chrome.runtime.connect({ name: "watching-page" });
+	backgroundPort.postMessage(message);
+	console.log("[Discord Watching Notifier] Sent message to background:", message.url, message.title);
+}
+
 switch (url) {
 	case "https://animestore.docomo.ne.jp/animestore/sc_d_pc":
-		watchingSitesStatus.url = "https://animestore.docomo.ne.jp/";
-		watchingSitesStatus.title = document.querySelectorAll(".backInfoTxt1")[0].textContent || "アニメを視聴中";
-		watchingSitesStatus.description = `${document.querySelectorAll(".backInfoTxt2")[0].textContent || ""} ${document.querySelectorAll(".backInfoTxt3")[0].textContent || ""}`;
+		// 少し待ってから実行しないと取得できなかった。多分描画されてない
+		setTimeout(() => {
+			watchingSitesStatus.url = "https://animestore.docomo.ne.jp/";
+			watchingSitesStatus.title = document.querySelector(".backInfoTxt1")?.textContent || "アニメを視聴中";
+			watchingSitesStatus.description = `${document.querySelector(".backInfoTxt2")?.textContent || "詳細情報"} ${document.querySelector(".backInfoTxt3")?.textContent || "不明"}`;
+			sendToBackground();
+		}, 1000);
+
 		break;
 	// case "https://www.amazon.co.jp/gp/video/detail":
 	// 	watchingSitesStatus.url = "https://www.amazon.co.jp/";
@@ -22,20 +44,3 @@ switch (url) {
 	default:
 		break;
 }
-
-// このページが matches に入ったので、URL と title を background に渡す。
-const message: PageMatchedMessage = {
-	type: "PAGE_MATCHED",
-	...watchingSitesStatus,
-}
-
-console.log("[Discord Watching Notifier] Page matched:", message.url, message.title);
-
-// content script からは native messaging を直接呼べないので、service worker に中継する。
-// タブが閉じられることを監視したいため、Service WorkerとContent Scriptをつなぐポートを作る。
-// ポートはページ遷移やタブ閉じで切断されるため、それで検知する
-const backgroundPort = chrome.runtime.connect({ name: "watching-page" });
-backgroundPort.postMessage(message);
-console.log("[Discord Watching Notifier] Sent message to background:", message.url, message.title);
-
-
